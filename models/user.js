729d403c79521c90a5fcda9d12c6b039cbe05e24
Sean Daly <daly.se91@gmail.com>
2021-03-02 19:06:09 -0600

    added password encryption back into user model

--- models/user.js
+++ models/user.js
@@ -14,13 +14,13 @@ const userSchema = new Schema(
     }
 )
 // mongoose pre-middleware: Pre middleware functions are executed one after another, when each middleware calls next.
-// userSchema.pre('save', async function(next) {
-//     // bcryptjs function. 
-//     if (this.isModified('password')){ //if password field is modified encrypt it using bcrypt. 
-//         //https://www.npmjs.com/package/bcryptjs
-//         this.password = await bcrypt.hash(this.password, 12)
-//     }
-//   });
+userSchema.pre('save', async function(next) {
+    // bcryptjs function. 
+    if (this.isModified('password')){ //if password field is modified encrypt it using bcrypt. 
+        //https://www.npmjs.com/package/bcryptjs
+        this.password = await bcrypt.hash(this.password, 12)
+    }
+  });
 
 const User = mongoose.model('user', userSchema)
 
