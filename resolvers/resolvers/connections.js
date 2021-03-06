003e76eb31a262d10b68f541637579e5bbce44c9
Sean Daly <daly.se91@gmail.com>
2021-03-06 16:43:22 -0600

    added destroyConnection, add and destroy connection from Group

--- resolvers/resolvers/connections.js
+++ resolvers/resolvers/connections.js
@@ -1,31 +1,69 @@
 const User = require('../../models/user')
 const Connections = require('../../models/connection')
+const Group = require('../../models/group')
+const user = require('../../typeDefs/typeDefs/user')
 const connections = {
     Mutation: {
         addConnection: async (parent, args, context, info)=>{
+            const {userId, connectionId} = args
             try{
-                const userA = await User.findByIdAndUpdate({_id: args.userId})
-                console.log(userA)
-                if (args.input==args.userId){
+                const user = await User.findByIdAndUpdate({_id: userId})
+                
+                if (connectionId==userId){
                     throw new Error('You cannot add yourself')
                 }
-                if (userA.connections.some(connection=>connection._id ==args.input)){
+                if (user.connections.some(connection=>connection._id == connectionId)){
                     throw new Error("You're already connected")
                 }
                 
-                await userA.connections.push(args.input)
-               return await userA.save()
-                
-        
-                
-        
-            }catch (error){ throw error}
-        }
+                await user.connections.push(connectionId)
+                return await user.save()
+            }
+            catch (error){ throw error}
+        },
+        destroyConnection: async (parent, args, context, info)=>{
+            const {userId, connectionId} = args
+            try{
+                const user = await User.findByIdAndUpdate({_id: userId})
+
+                const connectionIndex= user.connections.findIndex((ele)=>ele == connectionId) 
+
+                await  user.connections.splice(connectionIndex, 1)
+                return await user.save()
+
+            }
+            catch (error){ throw error}
+        },
+
+        addConnectionToGroup: 
+            async (parent, args, context, info)=>{
+                const {groupId, userId} = args
+                try{
+                    const group = await Group.findByIdAndUpdate({_id: groupId})
+                    // ********NEED TO ADD LOGIC FOR ADDING YOURSELF********************
+                    // if (userId==args.userId){
+                    //     throw new Error('You cannot add yourself')
+                    // }
+                    if (group.connections.some(connection=>connection._id ==user.Id)){
+                        throw new Error("${connection.name} is already in this group")
+                    }
+                    await group.connections.push(userId)
+                    
+                    return await group.save()
+
+                }catch (error){ throw error}
+            },
+        destroyConnectionFromGroup:
+            async (parent, args, context, info)=>{
+                const {groupId, userId} = args
+                try{
+                    const group = await Group.findByIdAndUpdate({_id: groupId})
+                   
+                    const connectionIndex= group.connections.findIndex((ele)=>ele == userId) 
+                    await group.connections.splice(connectionIndex, 1)
+                    return await group.save()
+                }catch (error){ throw error}
+            }
     }
-    // Connection: {
-    //     user: async(User)=>{
-    //         return (await User.populate('user').execPopulate()).User
-    //     }
-    // }
 }
 module.exports = connections
--- typeDefs/typeDefs/connections.js
+++ typeDefs/typeDefs/connections.js
@@ -2,14 +2,11 @@ const {gql} = require('apollo-server-express')
 
 const connections = gql`
     extend type Mutation{
-        addConnection(userId: ID!, input: ID!): User
+        addConnection(userId: ID!, connectionId: ID!): User
+        destroyConnection(userId: ID!, connectionId: ID!): User
+        addConnectionToGroup(groupId: ID!, userId: ID!): Group
+        destroyConnectionFromGroup(groupId: ID!, userId: ID!): Group
     }
-    
-    # type Connection {
-    #     _id: ID
-    #     user: User
-    # }
-    
 
     
 `
